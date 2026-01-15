a8286becbe2674351ed54fdaa5979a0701247320
Zoryko <alexandre.poulet71@gmail.com>
2026-01-15 19:22:07 +0100

    Update socket.ts

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io(process.env.REACT_APP_API_URL as string, {
+const socket: Socket = io(import.meta.env.VITE_API_URL as string, {
     autoConnect: false,
     transports: ['websocket']
 });
