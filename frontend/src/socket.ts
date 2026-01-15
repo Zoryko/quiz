0330deb7c1c5142916f7504e8d36c7424c9c81d7
Zoryko <alexandre.poulet71@gmail.com>
2026-01-15 18:44:32 +0100

    Update socket.ts

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,5 @@
 import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io("http://localhost:3001", {
 const socket: Socket = io("http://wrong-siana-alexp71eperso-8e0806a8.koyeb.app/", {
     autoConnect: false
 });
